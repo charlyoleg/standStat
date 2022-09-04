966f1ff5bb26d4d029be1893eb41fc950e83057d
charles <charles.braquet@e.email>
2022-09-04 21:24:05 +0200

    check if the path exists

--- static_webserver.js
+++ static_webserver.js
@@ -2,8 +2,10 @@
 
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import fs from 'node:fs';
 import express from 'express';
 
+// cli
 const argv = yargs(hideBin(process.argv))
   //.scriptName("standStat")
   .usage("Usage: $0 --port <port> --directoy <directory-path>")
@@ -25,13 +27,18 @@ const argv = yargs(hideBin(process.argv))
   .strict()
   .parse();
 
+// sanity checks
 if (argv.directory === '') {
   console.error(`ERR334: Error, you must specify a not empty directory-path!`);
   process.exit(-1);
 }
+if (! fs.existsSync(argv.directory)) {
+  console.error(`ERR339: Error, the path ${argv.directory} doesn't exist!`);
+  process.exit(-1);
+}
 
+// the main
 console.log(`INFO202: Serve on the port ${argv.port} the content of the directory ${argv.directory}`);
-
 const app = express();
 app.use(express.static(argv.directory));
 app.listen(argv.port);
